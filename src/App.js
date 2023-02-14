201bf7f2b64ff9d996f7512c5feb583a172f71d4
magdalenakosinska <59508924+magdalenakosinska@users.noreply.github.com>
2023-02-14 18:59:06 +0100

    created an array of 64 random colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import { useState } from "react";
+
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 function App() {
-  return <div className="App"></div>;
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+    for (let i = 0; i < width * width; i++) {
+      const randomColor =
+        candyColors[Math.floor(Math.random() * candyColors.length)];
+      randomColorArrangement.push(randomColor);
+    }
+
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+  createBoard();
+  return <div></div>;
 }
 
 export default App;
