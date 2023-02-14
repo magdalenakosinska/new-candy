4697edc765d6deded3a1b7e77b034761bc5b59e1
magdalenakosinska <59508924+magdalenakosinska@users.noreply.github.com>
2023-02-14 19:41:01 +0100

    created: checking for 3 colors in a column

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 function App() {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
 
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (number) => currentColorArrangement[number] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (number) => (currentColorArrangement[number] = "")
+        );
+      }
+    }
+  };
+
   const createBoard = () => {
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
@@ -20,11 +37,22 @@ function App() {
   useEffect(() => {
     createBoard();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+    }, 100);
+  }, [checkForColumnOfThree]);
+
   return (
     <div className="app">
       <div className="game">
         {currentColorArrangement.map((candyColor, index: number) => (
-          <img key={index} style={{ backgroundColor: candyColor }} />
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
         ))}
       </div>
     </div>
